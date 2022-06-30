3b1cb091bb3b453521f62245c5e8da844b8d2dd7
Seannie <shlim4915@gmail.com>
2022-07-01 08:42:08 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -73,9 +73,9 @@ function App() {
   const [id, setId] = useState(null);
   const [nextId, setNextId] = useState(4);
   const [topics, setTopics] = useState([
-    {id:1, title:'html', body:'html is ...'},
-    {id:2, title:'css', body:'css is ...'},
-    {id:3, title:'javascript', body:'javascript is ...'}
+    {id:1, title:'Seunghyeon', body:'Seunghyeon is ...'},
+    {id:2, title:'Seunghyeon2', body:'Seunghyeon2 is ...'},
+    {id:3, title:'Seunghyeon3', body:'Seunghyeon3 is ...'}
   ]);
   let content = null;
   let contextControl = null;
